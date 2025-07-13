8e1dc207ce1d0e3388fb66a06cf6c1c865bf225f
Abdirizack Hanshi <148985083+rizakhanshi@users.noreply.github.com>
2025-07-13 22:20:30 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,11 +1,11 @@
 # Define variables
-P = 1000  # Principal amount
-R = 0.05          # Interest rate (in %)
-T = 3          # Time (in years)
+principle = 1000  # Principal amount
+rate = 0.05          # Interest rate (in %)
+time = 3          # Time (in years)
 # Calculate simple interest
-I = (P * R * T)
+interest = (principle * rate * time)
 
 
 # Print the result     
 
-print("The simple interest is:", I)
+print("The simple interest is:", interest)
