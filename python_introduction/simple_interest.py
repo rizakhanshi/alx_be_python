378854b09112a33f20bb5e84e72831dd1e86eadc
Abdirizack Hanshi <hasawi009@gmail.com>
2025-07-13 21:47:25 +0300

    Add simple interest file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Define variables
+principal = 1000  # Principal amount
+rate = 5          # Interest rate (in %)
+time = 3          # Time (in years)
+# Calculate simple interest
+simple_interest = (principal * rate * time) / 100   
+
+# Print the result     
+
+print("The simple interest is:", simple_interest)
