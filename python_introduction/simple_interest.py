b28e1cb49b47b6a808524ea800284b02bca3c8a1
Abdirizack Hanshi <148985083+rizakhanshi@users.noreply.github.com>
2025-07-13 22:36:17 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,7 +3,7 @@ principal = 1000  # Principal amount
 rate = 0.05          # Interest rate (in %)
 time = 3          # Time (in years)
 # Calculate simple interest
-interest = (principal * rate * time)
+Interest = (principal * rate * time)
 
 
 # Print the result     
